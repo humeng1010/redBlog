ad2d9c52440391a27c9a0184fcc8f7cdf0010b63
redyouzi <1589029587@qq.com>
2022-09-19 22:43:26 +0800

    big modify admin pages

--- admin_page_plus/src/App.vue
+++ admin_page_plus/src/App.vue
@@ -1,19 +1,16 @@
 <template>
   <div id="app">
-    <AdminHeader v-show="$route.meta.show"></AdminHeader>
     <left v-show="$route.meta.show"></left>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
-import AdminHeader from "@/components/AdminHeader.vue";
 import Left from "@/components/Left.vue";
 export default {
   name: "App",
   components: {
     Left,
-    AdminHeader,
   },
   mounted() {
     console.dir(this.$route);
@@ -22,4 +19,28 @@ export default {
 </script>
 
 <style>
+body,
+html {
+  width: 100%;
+  height: 100%;
+}
+#app {
+  display: flex;
+  flex-wrap: wrap;
+  height: 100%;
+}
+
+/* ===重设样式=== */
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+a {
+  color: #333;
+  text-decoration: none;
+}
+li {
+  list-style: none;
+}
 </style>
--- admin_page_plus/src/pages/Home/index.vue
+++ admin_page_plus/src/pages/Home/index.vue
@@ -1,38 +1,5 @@
 <template>
-  <div>
-    <el-page-header @back="goBack" content="详情页面"> </el-page-header>
-    <el-carousel :interval="4000" type="card" height="200px">
-      <el-carousel-item v-for="item in images" :key="item.id">
-        <h3 class="medium">
-          <img class="img-size" :src="item.img" alt="轮播图片" />
-        </h3>
-      </el-carousel-item>
-    </el-carousel>
-    {{ msg }}页
-    <el-container>
-      <el-header style="text-align: right; font-size: 12px">
-        <el-dropdown>
-          <i class="el-icon-setting" style="margin-right: 15px"></i>
-          <el-dropdown-menu slot="dropdown">
-            <el-dropdown-item>查看</el-dropdown-item>
-            <el-dropdown-item>新增</el-dropdown-item>
-            <el-dropdown-item>删除</el-dropdown-item>
-          </el-dropdown-menu>
-        </el-dropdown>
-        <span>王小虎</span>
-      </el-header>
-
-      <el-main>
-        <el-table :data="tableData">
-          <el-table-column prop="date" label="日期" width="140">
-          </el-table-column>
-          <el-table-column prop="name" label="姓名" width="120">
-          </el-table-column>
-          <el-table-column prop="address" label="地址"> </el-table-column>
-        </el-table>
-      </el-main>
-    </el-container>
-  </div>
+  <div class="rigth"></div>
 </template>
 
 <script>
@@ -40,44 +7,11 @@ export default {
   name: "Home",
 
   data() {
-    const item = {
-      date: "2016-05-02",
-      name: "王小虎",
-      address: "上海市普陀区金沙江路 1518 弄",
-    };
-    return {
-      msg: "home",
-      tableData: Array(20).fill(item),
-      images: [
-        {
-          id: 1,
-          img: "../image/room.jpg",
-        },
-        {
-          id: 2,
-          img: "../image/wallhaven-9mjoy1_2560x1600.png",
-        },
-        {
-          id: 3,
-          img: "../image/wallhaven-k7v9yq.png",
-        },
-        {
-          id: 4,
-          img: "../image/wallhaven-rdl9vw.jpeg",
-        },
-      ],
-    };
+    return {};
   },
-  methods: {
-    goBack() {
-      console.log("go back");
-    },
-  },
+  methods: {},
 };
 </script>
 
 <style scoped>
-.img-size {
-  width: 100%;
-}
 </style>
